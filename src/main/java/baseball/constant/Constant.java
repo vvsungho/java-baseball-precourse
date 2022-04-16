590a3b377c5f677d13644fdf06d53420b4873f0b
shyoon <shyoon@12cm.co.kr>
2022-04-16 22:27:22 +0900

    feat(Constant.java): 상수 관리 클래스 생성

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,25 @@
+package baseball.constant;
+
+public class Constant {
+    public static final int NUMBER_LENGTH_VALUE = 3;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+    public static final int ANSWER_VALUE = 3;
+
+    public static final String NOT_ALLOW_NUMBER_VALUE = "0";
+    public static final String RESTART_GAME_VALUE = "1";
+    public static final String END_GAME_VALUE = "2";
+
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String INPUT_NUMBER_MESSAGE = "1 ~ 9 사이 숫자를 입력해주세요.";
+    public static final String INVALID_INPUT_MESSAGE = "잘못된 입력입니다.";
+    public static final String INVALID_INPUT_LENGTH_MESSAGE = "입력된 숫자의 자릿수가 올바르지 않습니다.";
+    public static final String INVALID_INPUT_DUPLICATION_MESSAGE = "중복된 숫자 없이 입력해주세요.";
+    public static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+
+    public static final String RESTART_MESSAGE = "게임을 새로 새직하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+}
