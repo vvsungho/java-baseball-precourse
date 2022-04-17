caad6f39f90d88066d4c2da9eedd109b78f3f754
shyoon <shyoon@12cm.co.kr>
2022-04-17 22:14:58 +0900

    refactor: View 클래스 불필요 함수 제거

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -10,24 +10,4 @@ public class OutputView {
     public static void printEndGame() {
         System.out.println(Constant.SUCCESS_MESSAGE);
     }
-
-    public static void printBall() {
-        System.out.println(Constant.BALL);
-    }
-
-    public static void printStrike() {
-        System.out.println(Constant.STRIKE);
-    }
-
-    public static void printNothing() {
-        System.out.println(Constant.NOTHING);
-    }
-
-    public static void printCount(int count) {
-        System.out.println(count);
-    }
-
-    public static void printSpacing() {
-        System.out.println(" ");
-    }
 }
