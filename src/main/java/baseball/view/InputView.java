d0a25f6fe18878108985371e6824f8e212587eee
shyoon <shyoon@12cm.co.kr>
2022-04-17 22:45:46 +0900

    refactor: View 클래스 불필요 함수 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,22 +9,6 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static void printInvalidInput() {
-        System.out.println(Constant.INVALID_INPUT_MESSAGE);
-    }
-
-    public static void printInvalidInputLength() {
-        System.out.println(Constant.INVALID_INPUT_LENGTH_MESSAGE);
-    }
-
-    public static void printInputNumberCondition() {
-        System.out.println(Constant.INPUT_NUMBER_MESSAGE);
-    }
-
-    public static void printInputDuplication() {
-        System.out.println(Constant.INVALID_INPUT_DUPLICATION_MESSAGE);
-    }
-
     public static void printRestart() {
         System.out.println(Constant.RESTART_MESSAGE);
     }
